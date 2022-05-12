aa524ee3fa8cf4a6784869375b0fe722bd0328b3
Jonathan Jara Assmann <j.jara21@ufromail.cl>
2022-05-11 21:38:33 -0400

    Add new pages

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,16 @@
+import { Box } from '@chakra-ui/react'
 import { DashMenu } from '../Components/Molecules/DashboardComponents/DashMenu'
 function Dashboard () {
   return (
-    <DashMenu />
+    <Box
+      marginInline={5}
+      marginBottom={20}
+      marginTop={3}
+      height='100vh'
+      backgroundColor='#fafafa'
+    >
+      <DashMenu />
+    </Box>
   )
 }
 
