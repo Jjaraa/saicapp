7579f82e02c65939dfcd261285bb569d5a18c3f3
Jonathan Jara Assmann <j.jara21@ufromail.cl>
2022-05-11 21:40:11 -0400

    Update DashMenu component

--- src/Components/Molecules/DashboardComponents/DashMenu/index.jsx
+++ src/Components/Molecules/DashboardComponents/DashMenu/index.jsx
@@ -1,7 +1,24 @@
+import {
+  Spacer,
+  Flex,
+  ButtonGroup
+} from '@chakra-ui/react'
+import { MenuLink } from '../../../Atoms/Menu/MenuLink'
 
 function DashMenu () {
   return (
-    <h1>Soy el menu del dashboard</h1>
+    <>
+      <Flex borderBlockEnd='1px'>
+        <></>
+        <Spacer />
+        <ButtonGroup gap={3} marginTop={3} marginRight={10}>
+          <MenuLink to='/'>Resumen</MenuLink>
+          <MenuLink to='/'>Inventario</MenuLink>
+          <MenuLink to='/'>Clientes</MenuLink>
+          <MenuLink to='/'>Cotizaciones</MenuLink>
+        </ButtonGroup>
+      </Flex>
+    </>
   )
 }
 
