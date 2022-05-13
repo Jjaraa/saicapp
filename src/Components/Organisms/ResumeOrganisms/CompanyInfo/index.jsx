32edc3edef3fd712b94926ab6b919a795002847d
Jonathan Jara Assmann <j.jara21@ufromail.cl>
2022-05-12 22:40:22 -0400

    Add company organism

---
+++ src/Components/Organisms/ResumeOrganisms/CompanyInfo/index.jsx
@@ -0,0 +1,58 @@
+import {
+  Flex,
+  Box
+} from '@chakra-ui/react'
+import { useState } from 'react'
+import { PrimaryButton } from '../../../Atoms/Button/PrimaryButton'
+import { SecondaryButton } from '../../../Atoms/Button/SecondaryButton'
+import { CompanyInfoArea } from '../../../Molecules/ResumeMolecules/CompanyInfoArea'
+import { CompanyEditArea } from '../../../Molecules/ResumeMolecules/CompanyEditArea'
+import { Title } from '../../../Atoms/Text/Title'
+
+// feed para la empresa
+const feedCompany = {
+  name: 'Empresa uno',
+  rut: '12345678-9',
+  giro: 'Electrónica',
+  address: 'Av. Las Flores 123'
+}
+
+function CompanyInfo () {
+  const [isEditing, setIsEditing] = useState(false)
+  const [company, setCompany] = useState(feedCompany)
+  const [newCompany, setNewCompany] = useState({})
+
+  function handleCallback (_company) {
+    setNewCompany(_company)
+  }
+
+  return (
+    <Box w='50%'>
+      <Flex gap={3}>
+        <Title>Datos de la empresa</Title>
+        <>
+          {
+            isEditing
+              ? <>
+                <SecondaryButton onClick={() => setIsEditing(false)}>Cancelar</SecondaryButton>
+                <PrimaryButton onClick={() => {
+                  setCompany(newCompany)
+                  setIsEditing(false)
+                }}
+                >Guardar
+                </PrimaryButton>
+                </> // eslint-disable-line
+              : <SecondaryButton onClick={() => setIsEditing(true)}>Editar</SecondaryButton>
+          }
+        </>
+      </Flex>
+      {
+        isEditing
+          ? <CompanyEditArea company={company} handleCallback={handleCallback} />
+          : <CompanyInfoArea company={company} />
+      }
+    </Box>
+  )
+}
+
+export { CompanyInfo }
